fb07343dba4982e974264a30d1cd4b1394dae993
MiguelSantos1910 <miguelsantos51346@gmail.com>
2026-04-25 17:15:15 -0300

    Delete Backend directory

--- Backend/models/Clp.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const ClpSchema = new mongoose.Schema({
-  timestamp: { type: Date, default: Date.now },
-  db: String,
-  tag: String,
-  value: mongoose.Schema.Types.Mixed
-});
-module.exports = mongoose.model("ClpData", ClpSchema);
